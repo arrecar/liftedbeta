4d2825ce89c51e85cdbd69baefe5568bd093afe9
Poser <arr15004@byui.edu>
2023-06-24 12:10:17 -0600

    updated navbar
    updated navbar so it shows differente links in the menu depending on the role of the user that logs in

--- frontend/src/components/navbar/navbar.js
+++ frontend/src/components/navbar/navbar.js
@@ -1,18 +1,43 @@
 import React from 'react'
 
 function Navbar(props) {
-    
-    return ( 
+
+    if (props.role.toLowerCase() === "administrator") {
+        return(
+            <div className='navbar'>
+                <img src='../../../src/pictures/logo.png' alt = "Lifted Beta logo"></img>
+                <div className='navlist'>
+                    <div></div><button>Users</button >
+                    <div></div><button>Students</button >
+                    <div></div><button>Grades</button >
+                    <div></div><button>Transcripts</button >
+                </div>
+            </div> 
+        );
+    }
+    else if (props.role.toLowerCase() === "parent") {
+        return ( 
         <div className='navbar'>
             <img src='../../../src/pictures/logo.png' alt = "Lifted Beta logo"></img>
             <div className='navlist'>
-                <div><img src="../../../src/pictures/navbar/users.png"></img></div><button>Users</button >
-                <div></div><button>Students</button >
                 <div></div><button>Grades</button >
                 <div></div><button>Transcripts</button >
             </div>
-    </div> 
-    );
+        </div> 
+        );
+    }
+    else if (props.role.toLowerCase() === "teacher"){
+        return(
+            <div className='navbar'>
+                <img src='../../../src/pictures/logo.png' alt = "Lifted Beta logo"></img>
+                <div className='navlist'>
+                    <div></div><button>Students</button >
+                    <div></div><button>Grades</button >
+                    <div></div><button>Transcripts</button >
+                </div>
+            </div> 
+        );
+    }
 }
 
 export default Navbar;
