55ecd5c23990e95103ca0bf6693fdd3679ca25ce
Poser <arr15004@byui.edu>
2023-07-06 10:57:28 -0600

    production
    Added code so the page can go in production and try it out to be live on render.com

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 const userRoutes = require('./routes/userRoutes');
 const {notFound, errorHandler} = require('./middlewares/errorMiddleware');
+const path = require('path');
 
 
 
@@ -13,9 +14,7 @@ connectDB();
 app.use(express.json());
 
 
-app.get('/', (req, res) => {
-    res.send("API is running..")
-});
+
 
 /*app.get('/api/users', (req, res) => {
     res.json(users)
@@ -27,6 +26,22 @@ app.get('/', (req, res) => {
 });*/
 
 app.use('/api/users', userRoutes)
+
+//-------------deployment-------------
+
+__dirname=path.resolve();
+
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname,'/frontend/build')));
+
+    app.get('*',(req,res) => {
+        res.sendFile(path.resolve(__dirname,'frontend','buidl','index.html'))
+    })
+}else{
+    app.get('/', (req, res) => {
+        res.send("API is running..")
+    });
+}
 
 app.use(notFound);
 app.use(errorHandler);
